9f4f4f633952d57b65faa95b7492995aa08c323e
Alejandro Bertinelli <alexberti02@gmail.com>
2025-04-29 16:13:54 +0100

    Create basic item models

---
+++ BlazorApp/Data/GoodsItem.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp.Data
+{
+    public class GoodsItem
+    {
+
+        public required string Name { get; set; }
+
+        public ImportStatus ImportStatus { get; set; }
+
+        public decimal PriceBeforeTax { get; set; }
+
+    }
+}
---
+++ BlazorApp/Data/ImportStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Data
+{
+    public enum ImportStatus
+    {
+        [Display(Name = "Please select whether the item was imported or not")]
+        Unknown,
+        [Display(Name = "Not Imported")]
+        NotImported,
+        [Display(Name = "Imported")]
+        Imported
+    }
+}
